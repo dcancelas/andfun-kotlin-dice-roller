c857aea20fae3f52d3e91ab2b7baa2c050ff542e
dcancelas <55793127+DanielCancelas@users.noreply.github.com>
2020-10-13 10:36:00 +0200

    Se ha aplicado la traducción al español

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -39,8 +39,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
